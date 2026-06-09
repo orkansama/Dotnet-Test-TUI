2e1405c191f7fba24c2602ced534e9d1993965c7
OrkanGoekcen <orkan.gok@gmail.com>
2026-06-09 12:44:04 +0200

    feat: added right window

--- main.c
+++ main.c
@@ -27,6 +27,23 @@ WINDOW *LeftWin(int top_win_height, int max_y, int max_x) {
   return win_left;
 }
 
+WINDOW *RightWin(int top_win_height, int max_y, int max_x) {
+  int half_x = max_x / 2;
+
+  int winHeight = max_y - top_win_height;
+  int winWidth = max_x / 2;
+  int winStartTop = top_win_height;
+  int winStartLeft = max_x / 2;
+
+  WINDOW *win_right = newwin(winHeight, winWidth, winStartTop, winStartLeft);
+
+  box(win_right, 0, 0);
+  refresh();
+  wrefresh(win_right);
+
+  return win_right;
+}
+
 int main(void) {
   initscr();
   cbreak();
@@ -40,11 +57,13 @@ int main(void) {
   int TOP_WIN_HEIGHT = 3;
   WINDOW *win_top = TopWin(TOP_WIN_HEIGHT, max_x);
   WINDOW *win_left = LeftWin(TOP_WIN_HEIGHT, max_y, max_x);
+  WINDOW *win_right = RightWin(TOP_WIN_HEIGHT, max_y, max_x);
 
   getch();
 
   delwin(win_top);
   delwin(win_left);
+  delwin(win_right);
   endwin();
   return 0;
 }
